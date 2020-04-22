a48336055650bb1feafdfb3d28f133cef4dd08ae
Jang soyeon <jerry1004@abc.com>
2020-04-22 13:07:22 +0900

    Create manager.h

---
+++ manager.h
@@ -0,0 +1,8 @@
+#include "product.h"
+
+int selectMenu();
+void listProduct(Product *p,int count);
+int selectDataNo(Product *p, int count);
+
+void saveData(Product *p, int count);
+int loadData(Product *p);
