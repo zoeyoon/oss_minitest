ea78db8d325823c1ec44644210fa67317d12a7ad
zoeyoon <yoonzoeym27@gmail.com>
2023-04-23 15:30:41 +0900

    saveFile() 구현

--- manager.c
+++ manager.c
@@ -36,16 +36,19 @@ int selectDataNo(Product *p, int count){
 
 //배열데이터를 파일에 저장하는 함수
 void saveData(Product p[], int count){
-	FILE* fp;
+        FILE* fp;
 
-	//중량 가격 제품명
-	fp= fopen("product.txt","wt");
-	
-	
-	fclose(fp);
-	printf("저장됨!\n");
+        //중량 가격 제품명
+        fp= fopen("product.txt","wt");
+
+        for (int i=0; i<count; i++) {
+                if(p[i].weight==-1 || p[i].price==-1) continue;
+                fprintf(fp, "%d %d %s\n" ,p[i].weight, p[i].price, p[i].name);
+        }
+
+        fclose(fp);
+        printf("저장됨!\n");
 }
-
 
 //파일에서 데이터 불러오는 함수
 int loadData(Product *p){
